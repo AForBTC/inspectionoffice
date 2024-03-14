5a5c29861864ab8d94f54d2a23d468a9288fe201
dihaichao <dhc@qq.com>
2024-03-14 16:56:15 +0800

    Initial commit

---
+++ src/main/java/com/ws/inspectionoffice/InspectionofficeApplication.java
@@ -0,0 +1,13 @@
+package com.ws.inspectionoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspectionofficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InspectionofficeApplication.class, args);
+    }
+
+}
